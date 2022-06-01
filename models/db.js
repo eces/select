f984e7c614077f143871026b1b1fd58edd32a968
Emerson Almeida <emersonalmeidax@gmail.com>
2022-06-01 01:37:00 -0300

    Accept DATABASE URL

--- models/db.js
+++ models/db.js
@@ -29,14 +29,23 @@ module.exports.init = async () => {
         },
       })
     } else if (r.type == 'postgres') {
+      let connectionAttributes;
+
+      if (r.url) {
+        connectionAttributes = { url: r.url }
+      } else {
+        connectionAttributes = {
+          host: r.host,
+          port: r.port || 5432,
+          username: r.username,
+          password: Buffer.from(r.password, 'base64').toString('utf-8'),
+          database: r.database,
+        }
+      }
+
       await createConnection({
         name: r.key,
         type: 'postgres',
-        host: r.host,
-        port: r.port || 3306,
-        username: r.username,
-        password: Buffer.from(r.password, 'base64').toString('utf-8'),
-        database: r.database,
         synchronize: false,
         logging: process.env.NODE_ENV == 'development' ? true : false,
         connectTimeoutMS: Math.max(10*1000, r.connectTimeoutMS || 10*1000),
